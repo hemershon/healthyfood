6d78b6a48c9cb6492db2a65d312fb116c4d11288
Hemershon Silva <hemershon@gmail.com>
2022-08-30 14:12:00 -0300

    feat(build) create reviews

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id            :bigint           not null, primary key
+#  value         :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  restaurant_id :bigint           not null
+#
+# Indexes
+#
+#  index_reviews_on_restaurant_id  (restaurant_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (restaurant_id => restaurants.id)
+#
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
