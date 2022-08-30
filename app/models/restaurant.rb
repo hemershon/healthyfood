5a57d9755caa8620705b06589b31998265042721
Hemershon Silva <hemershon@gmail.com>
2022-08-30 11:44:28 -0300

    feat(build) create validates and enum restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -9,6 +9,8 @@ class Restaurant < ApplicationRecord
   validates :status, presence: true
   validates :delivery_tax, presence: true
   validates :city, presence: true
-  validates :street, presence:true 
+  validates :street, presence:true
+
+  enum status: { closed: 0, open: 1 }
 
 end
