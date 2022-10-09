21db557ff3e3334d5d13ff67b297bdadf55286fa
Hemershon Silva <hemershon@gmail.com>
2022-10-09 18:10:41 -0300

    feat(build) create seed for test

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,3 +1,34 @@
+# == Schema Information
+#
+# Table name: restaurants
+#
+#  id           :bigint           not null, primary key
+#  cep          :string
+#  city         :string
+#  complement   :string
+#  delivery_tax :float
+#  description  :text
+#  latitude     :float
+#  longitude    :float
+#  name         :string
+#  neighborhood :string
+#  number       :string
+#  reference    :string
+#  state        :string
+#  status       :integer
+#  street       :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  category_id  :bigint           not null
+#
+# Indexes
+#
+#  index_restaurants_on_category_id  (category_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (category_id => categories.id)
+#
 class RestaurantSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
